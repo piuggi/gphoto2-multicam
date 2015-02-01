c74da67e5dde120e442d10a39b509d34a3a4bee0
cp <chris@piuggi.com>
2015-02-01 18:46:13 -0500

    adding image thumbnails on socket load and when images are pushed into folder.

--- source/js/socket.js
+++ source/js/socket.js
@@ -1,4 +1,12 @@
-var socket = function(){
-  var hello = '132';
-  console.log(hello);
-}();
+var socket = io.connect('http://localhost:8081');
+socket.on('images', function(images){
+  var imgDoc = document.getElementsByClassName("image")[0];
+  var imagesHolder = document.getElementsByClassName("images")[0];
+  for(var i =0, len = images.length; i<len;i++){
+    var imgClone = imgDoc.cloneNode(true);
+    imgClone.getElementsByTagName('img')[0].src = 'images/'+images[i];
+    imagesHolder.insertBefore(imgClone,imagesHolder.firstChild);
+  }
+  console.log(imgDoc);
+
+});
--- source/js/view.js
+++
@@ -1,6 +0,0 @@
-var view = function(){
-  var hello = '132';
-  console.log(hello);
-}();
-
-console.log('Testing.');
