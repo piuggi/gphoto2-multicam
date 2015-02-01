73acb80da7b075a78436e5da1e482c969e68ac41
cp <chris@piuggi.com>
2015-02-01 18:45:13 -0500

    Adding Express.js and Socket.io to enable live images updates on frontend

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
+var fs = require('fs');
 var gulp = require('gulp');
-var watcher = gulp.watch('images/*.jpg');
+/* Simple Express and Socket to pass info. */
+var express =require('express');
+var app = express();
+var server = require('http').Server(app)
+var io = require('socket.io')(server);
 
-watcher.on('change',function(event){
-    console.log('File Changed');
-    console.log(event);
-});
 
 var Gphoto2 = require('./components/gphoto2');
 
+if (!fs.existsSync('./images')) fs.mkdirSync('./images');
+
+/* Gulp Watcher */
+var watcher = gulp.watch('images/*.jpg');
+
+watcher.on('change',function(event){
+
+    switch(event.type){
+      case 'added':
+        console.log('File Added: '+event.path);
+        io.sockets.emit('images',[event.path.substr(event.path.lastIndexOf('/')+1)]);
+        break;
+      default:
+        console.log(event.type);
+        break;
+    }
+});
+
 var gphoto = new Gphoto2({},function(){
   gphoto.tetherAll();
+});
+
+/* Simple Express and Socket to pass info. */
+app.use(express.static(__dirname + '/public'));
+app.use('/images',express.static(__dirname +'/images'));
+app.listen(process.env.PORT || 8080);
+server.listen(8081);
+console.log('App Running on port'+(process.env.PORT || 8080));
+
+
+io.on('connection',function(socket){
+  fs.readdir('./images',function(err,files){
+    if(err) return socket.emit('error',err);
+    return socket.emit('images',files);
+  });
 });
