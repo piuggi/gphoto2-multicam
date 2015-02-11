5beb13ff9141d474196280221677815920fb5287
cp <chris@piuggi.com>
2015-02-11 01:30:59 -0500

    adding fix for incorrect take

--- app/models/images.js
+++ app/models/images.js
@@ -15,7 +15,7 @@ var Images = mongoose.Schema({
 
 Images.pre('save',function(next){
 
-  this.take = this.path.substr(0,1);
+  this.take = this.path.substr(0,this.path.indexOf('_'));
   this.camera = this.path.substr(this.path.indexOf('_')+1,1);
   next();
 });
